e99a81fc3c358898eff7b577b0900991eee556ca
Hezakai <graves.eric@gmail.com>
2023-02-27 16:06:02 -0600

    updated reaction and index

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const reactionSchema = new mongoose.Schema({
   reactionID: { type: ObjectId, required: true }, //Default value is set to a new ObjectId
-  reactionBody: { type: String, required: true}, //add 280 character limit
-  username: { type: String, required: true },
-  friends: { type: Array}, // references the user model?  
-  createdAt: { type: Date, default: Date.now, required: true }, //getter to format??
+  reactionBody: { type: String, required: true, maxlength: 280}, //no minlength?
+  username: { type: String, required: true }, 
+  createdAt: { type: Date, default: Date.now, required: true, get: createdAtVal => dateFormat(createdAtVal) }, //getter to format??
 });
 
 //This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
 
-module.exports = reactionSchema;
+function dateFormat(date) {
+  return new Date(date).toLocaleDateString();
+}
+
+module.exports = Reaction;  //no seeing Reaction?
